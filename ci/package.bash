9820433ff8027c40e545b9fda1996a29e617d76e
Sam <samsface@gmail.com>
2022-01-18 11:55:45 +0000

    ci fix

--- ci/package.bash
+++ ci/package.bash
@@ -1,3 +1,3 @@
-cp $1/*/lib/*.so    addons/steam_api/
-cp $1/*/lib/*.dll   addons/steam_api/
-cp $1/*/lib/*.dylib addons/steam_api/
+cp $1/*/lib/*.so    addons/discord_sdk/
+cp $1/*/lib/*.dll   addons/discord_sdk/
+cp $1/*/lib/*.dylib addons/discord_sdk/
