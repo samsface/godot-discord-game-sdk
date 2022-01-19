48997f380d75db01fe636d0101c2065712e55ea4
Sam <samsface@gmail.com>
2022-01-19 18:57:25 +0000

    rename to discord game sdk

--- dev.sh
+++ dev.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#git submodule update --init --recursive
-
 mkdir -p work
 cd work
 cmake .. -DCMAKE_BUILD_TYPE=Debug
-cmake --build . --target install ../app/addons/discord_sdk/
+cmake --build . --target install ../app/addons/discord_game_sdk/
