b6fb6950882bd9c32195f73fdb02b7e0601959ff
Sam <samsface@gmail.com>
2022-01-19 01:32:51 +0000

    remove stuff

--- app/game.gd
+++ app/game.gd
@@ -7,20 +7,20 @@ func _ready() -> void:
 	game_state_.connect("members_changed", self, "_on_members_changed")
 
 func _on_chat_changed() -> void:
-	$chat.text = ""
+	find_node("chat").text = ""
 	for chat_message in game_state_.chat:
 		var member = game_state_.members.get(chat_message.member_id)
 		if not member:
 			return
-		$chat.text += "\n" + member.username + ": " + chat_message.message
+		find_node("chat").text += "\n" + member.username + ": " + chat_message.message
 
-	$chat.scroll_vertical = 9999999
+	find_node("chat").scroll_vertical = 9999999
 
 func _on_members_changed() -> void:
-	$members.text = ""
+	find_node("members").text = ""
 	for member in game_state_.members.values():
-		$members.text += member.username + "\n"
+		find_node("members").text += member.username + "\n"
 
 func _on_text_entered(new_text):
 	$lobby.send_message(new_text)
-	$line_edit.clear()
+	find_node("line_edit").clear()
